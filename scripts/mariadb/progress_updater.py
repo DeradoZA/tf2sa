90be4dae19ae8332082450e58f0774fe1547ee25
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-06-19 02:48:58 +0200

    update python script to pull secrets from environment variables.

---
+++ scripts/mariadb/progress_updater.py
@@ -0,0 +1,113 @@
+import json, requests, mysql.connector, sys
+from steam.steamid import SteamID
+from dotenv import dotenv_values
+from schema import *
+import datetime
+import time
+
+WINDOW = 6*60*60*24*30
+MIN_MATCHES = 10
+
+def get_Overall(cursor, low, count, MIN_MATCHES):
+    res = cursor.execute("""
+    SELECT p.SteamID, a.Matches, ROUND(b.Hours, 1) Hours, ROUND(IFNULL(c.Kills, 0), 2) AS 'Kills', ROUND(IFNULL(c.Deaths, 0),2) AS 'Deaths', ROUND(IFNULL(c.Assists, 0),2) AS 'Assists', ROUND(IFNULL(e.Backstabs, 0),1) AS 'Backstabs', ROUND(IFNULL(f.Headshots, 0),1) AS 'Headshots', ROUND(IFNULL(d.Airshots, 0),2) AS 'Airshots', ROUND(IFNULL(c.DPM, 0),2) AS 'DPM', ROUND(a.DTM, 2) DTM, ROUND(a.HRM ,2) HRM FROM Players p
+    LEFT JOIN
+    (
+            SELECT p.SteamID, COUNT(p.GameID) AS 'Matches', SUM(p.DamageTaken) * 60 / SUM(g.Duration) AS 'DTM', SUM(p.HealsReceived) * 60 / SUM(g.Duration) AS 'HRM' FROM Games g
+            JOIN PlayerStats p ON g.GameID = p.GameID
+      WHERE Date > {low} AND Date < {high}
+            GROUP BY p.SteamID
+    ) AS a ON a.SteamID = p.SteamID
+    LEFT JOIN
+    (
+            SELECT p.SteamID, SUM(c.Playtime) / 3600 AS 'Hours' FROM Games g
+            JOIN PlayerStats p ON g.GameID = p.GameID
+            JOIN ClassStats c ON c.PlayerStatsID = p.PlayerStatsID
+      WHERE Date > {low} AND Date < {high}
+      GROUP BY p.SteamID
+    ) AS b ON b.SteamID = p.SteamID
+    LEFT JOIN
+    (
+            SELECT p.SteamID, SUM(c.Kills) / Count(DISTINCT(g.GameID)) AS 'Kills', SUM(c.Deaths) / Count(DISTINCT(g.GameID)) AS 'Deaths', SUM(c.Assists) / Count(DISTINCT(g.GameID)) AS 'Assists', SUM(c.Damage) * 60 / SUM(c.Playtime) AS 'DPM' FROM Games g
+            JOIN PlayerStats p ON g.GameID = p.GameID
+            JOIN ClassStats c ON c.PlayerStatsID = p.PlayerStatsID
+            WHERE c.ClassID != 7 AND Date > {low} AND Date < {high}
+            GROUP BY p.SteamID
+    ) AS c ON c.SteamID = p.SteamID
+    LEFT JOIN
+    (
+            SELECT p.SteamID, Sum(Airshots) / Count(DISTINCT(g.GameID)) AS 'Airshots' FROM Games g
+            JOIN PlayerStats p ON g.GameID = p.GameID
+            JOIN ClassStats c ON c.PlayerStatsID = p.PlayerStatsID
+            WHERE Date > {low} AND Date < {high} AND ClassID IN (2,4)
+            GROUP BY p.SteamID
+    ) AS d ON d.SteamID = p.SteamID
+    LEFT JOIN
+    (
+            SELECT p.SteamID, Sum(Backstabs) / Count(g.GameID)  AS 'Backstabs' FROM Games g
+            JOIN PlayerStats p ON g.GameID = p.GameID
+            JOIN ClassStats c ON c.PlayerStatsID = p.PlayerStatsID
+            WHERE ClassID = 9 AND Date > {low} AND Date < {high}
+            GROUP BY p.SteamID
+    ) AS e ON e.SteamID = p.SteamID
+    LEFT JOIN
+    (
+            SELECT p.SteamID, Sum(Headshots) / Count(g.GameID)  AS 'Headshots' FROM Games g
+            JOIN PlayerStats p ON g.GameID = p.GameID
+            JOIN ClassStats c ON c.PlayerStatsID = p.PlayerStatsID
+            WHERE ClassID = 8 AND Date > {low} AND Date < {high}
+            GROUP BY p.SteamID
+    ) AS f ON f.SteamID = p.SteamID
+    HAVING Matches > {min_matches}
+    ORDER BY DPM DESC
+    """.format(low=low, high=count, min_matches=MIN_MATCHES))
+    return res
+
+def updateProgress(cursor, reset=False) :
+    if reset == True:
+        cursor.execute("DELETE FROM Progress")
+    print("updating player progress...", end="")
+    #set start and end dates
+    cursor.execute("SELECT MIN(Date) FROM Games")
+    unix_first  = int(cursor.fetchall()[0][0]) + 60*60*24*30;
+    unix_now    = time.time()
+    date_first  = datetime.datetime.utcfromtimestamp(unix_first)
+    date_now    = datetime.datetime.utcfromtimestamp(unix_now)
+
+    startyear   = date_first.year
+    startmonth  = date_first.month + 1
+    endyear     = date_now.year
+    endmonth    = date_now.month
+    dates = [time.mktime(datetime.date(m//12, m%12+1, 1).timetuple()) for m in range(startyear*12+startmonth-1, endyear*12+endmonth)]
+
+    for date in dates:
+        check = cursor.execute("SELECT COUNT(*) FROM Progress WHERE EndDate = {}".format(int(date)))
+        check = cursor.fetchone()[0]
+        if check == 0:
+            print('...',datetime.datetime.utcfromtimestamp(date).strftime('%Y-%m-%d'), end="")
+            low = date - WINDOW
+            res = get_Overall(cursor, low, int(date), MIN_MATCHES)
+            res = cursor.fetchall()
+            for r in res:
+                cursor.execute("""INSERT INTO Progress
+                (EndDate, SteamID, Matches, Hours, Kills, Deaths, Assists, Backstabs, Headshots, Airshots, DPM, DTM, HRM)
+                VALUES
+                ({},{},{},{},{},{},{},{},{},{},{},{},{})
+                """.format(int(date), r[0], r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9],r[10],r[11]))
+    print("done")
+
+if __name__ == "__main__":
+    env = dotenv_values("../.env")
+    db = mysql.connector.connect(
+        host        = "localhost",
+        user        = env['MYSQL_USR'],
+        password    = env['MYSQL_PWD'],
+        database    = env['MYSQL_DB'],
+    )
+    cursor = db.cursor(buffered=True)
+    cursor.autocommit = True
+    DBInit(cursor)
+
+    updateProgress(cursor)
+
+    db.commit()
--- scripts/mariadb/schema.py
+++ scripts/mariadb/schema.py
@@ -1,226 +1,231 @@
-#update site database
-#Usage:
-#mysql_upater -> create db if not present and update all games
-#mysql_updater 50 -> limit number of logs updated to 50
-#mysql_updater 50 stat/forum->now also wipe stats/forum tables and recreate(MUST SUPPLY NUM)
-#NB MUST SUPPLY NUMBER IN LAST EXAMPLE.
-import json, requests, mysql.connector, sys
-from progress_updater import *
-from schema import *
-from steam.steamid import SteamID
-from dotenv import dotenv_values
-#UPDATE GAME STATS FROM LOGSTF
-TEAM_ID = {
-    "Blue"  : 0,
-    "Red"   : 1
-}
-
-CLASS_ID = {
-    "special"       : 0, #Tracks overall stats that can't be attributed to one class
-    "scout"         : 1,
-    "soldier"       : 2,
-    "pyro"          : 3,
-    "demoman"       : 4,
-    "heavyweapons"  : 5,
-    "engineer"      : 6,
-    "medic"         : 7,
-    "sniper"        : 8,
-    "spy"           : 9
-}
-
-#THRESHOLDS FOR IGNORING GAMES
-MIN_GAME_LENGTH = 15
-DMG_THRESH = 17500
-HPM_THRESH = 1300
-
-def UpdatePlayerNames(cursor):
-    Steam_API_Key = env["STEAM_API_KEY"]
-    cursor.execute("SELECT SteamID from Players")
-    SteamIDTuple = cursor.fetchall()
-    IDList = []
-
-    print("Updating player steam usernames.")
-
-    for ID in range(0, len(SteamIDTuple)):
-        IDList.append(SteamIDTuple[ID][0])
-
-        if len(IDList) == 100 or ID == len(SteamIDTuple):
-            PlayerInfo = json.loads(requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}".format(Steam_API_Key, IDList)).text)
-
-            UpdatedPlayerNames = {}
-
-            for Player in PlayerInfo["response"]["players"]:
-                UpdatedPlayerNames[Player["steamid"]] = Player["personaname"]
-
-            for SteamID, PlayerName in UpdatedPlayerNames.items():
-                qry = "UPDATE Players SET PlayerName = %s WHERE SteamID = %s"
-                cursor.execute(qry,(PlayerName, SteamID))
-
-            IDList = []
-            UpdatedPlayerNames = {}
-
-    print("done")
-
-#deletes game, adds to blacklist
-def BlackListLog(LogID, reason, cursor):
-    cursor.execute("INSERT INTO BlacklistGames (GameID, Reason) VALUES({},'{}')".format(LogID, reason))
-    cursor.execute("DELETE FROM Games WHERE GameID = {}".format(LogID))
-    print(reason, '. game blacklisted')
-
-#Add Player if not added.
-def AddPlayer(steamid, name, cursor):
-    cursor.execute("Select SteamID from Players where SteamID = {}".format(steamid))
-    PlayerExists = cursor.fetchone()
-    if PlayerExists:
-        return
-    else:
-        qry = "INSERT INTO Players VALUES (%s, %s)"
-        cursor.execute(qry,(steamid, name))
-
-def AddGame(LogID, Log, cursor):
-    global MIN_GAME_LENGTH
-    if (Log["length"] / 60) < MIN_GAME_LENGTH:
-        BlackListLog(LogID,"match too short", cursor)
-        return
-
-    #Add Game Record
+def DBInit(cursor):
+    print("confirming/recreating tables... ", end='')
     cursor.execute("""
-    INSERT INTO Games
-    VALUES ({},{},{},'{}',{},{})
-    """.format(LogID, Log["info"]["date"], Log["info"]["total_length"], Log["info"]["map"], Log["teams"]["Blue"]["score"], Log["teams"]["Red"]["score"]))
+    CREATE TABLE IF NOT EXISTS Users(
+        UserID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        SteamID BIGINT UNSIGNED,
+        UserName VARCHAR(32) NOT NULL,
+        PassHash CHAR(64) NOT NULL,
+        SessionID CHAR(64),
+        JoinDate INT UNSIGNED,
+        PRIMARY KEY(UserID, Username)
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
 
-    #Update Players
-    for PlayerID, Name in Log["names"].items():
-        AddPlayer(SteamID(PlayerID).as_64, Name, cursor)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Threads (
+        ThreadID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        UserID INT UNSIGNED NOT NULL,
+        Topic VARCHAR(300),
+        Date INT UNSIGNED,
+        PRIMARY KEY (ThreadID),
+        CONSTRAINT `fk_user_id`
+            FOREIGN KEY (UserID) REFERENCES Users (UserID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
 
-    global CLASS_ID
-    global TEAM_ID
-    global DMG_THRESH
-    global HPM_THRESH
-    for Player,PlayerStats in Log["players"].items():
-        #PLAYERSTATS
-        PlayerID        = SteamID(Player).as_64
-        TeamID          = TEAM_ID[PlayerStats["team"]]
-        DamageTaken     = PlayerStats["dt"]
-        HealsReceived   = PlayerStats["hr"]
-        MedkitsHP       = PlayerStats["medkits_hp"]
-        Airshots        = PlayerStats["as"]
-        Headshots       = PlayerStats["headshots_hit"]
-        Backstabs       = PlayerStats["backstabs"]
-        Drops           = PlayerStats["drops"]
-        Heals           = PlayerStats["heal"]
-        Ubers           = PlayerStats["ubers"]
-        Damage          = PlayerStats["dmg"] #stored here for validation
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Comments (
+        CommentID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        ThreadID INT UNSIGNED NOT NULL,
+        UserID INT UNSIGNED,
+        Date INT UNSIGNED,
+        Content TEXT(21000),
+        PRIMARY KEY (CommentID),
+        CONSTRAINT `fk_thread_id`
+            FOREIGN KEY (ThreadID) REFERENCES Threads (ThreadID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+        CONSTRAINT `fk_user_comment_id`
+            FOREIGN KEY (UserID) REFERENCES Users (UserID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
 
-        #1st validity check
-        if Damage > DMG_THRESH:
-            BlackListLog(LogID,"damage thresh exceeded", cursor)
-            return
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Images (
+        ImageID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        CommentID INT UNSIGNED NOT NULL,
+        ImageHash CHAR(64) NOT NULL,
+        PRIMARY KEY (ImageID),
+        CONSTRAINT `fk_image_commentid`
+            FOREIGN KEY (CommentID) REFERENCES Comments (CommentID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
 
-        #Add PlayerStats record
-        cursor.execute("""INSERT INTO PlayerStats
-        (GameID, SteamID, TeamID, DamageTaken, HealsReceived, MedkitsHP, Airshots, Headshots, Backstabs, Drops, Heals, Ubers)
-        VALUES ({},{},{},{},{},{},{},{},{},{},{},{})""".format(
-        LogID,PlayerID,TeamID,DamageTaken,HealsReceived,MedkitsHP,Airshots,Headshots,Backstabs,Drops,Heals,Ubers))
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Polls (
+        PollID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        CommentID INT UNSIGNED NOT NULL,
+        Topic VARCHAR(300),
+        PRIMARY KEY (PollID),
+        CONSTRAINT `fk_poll_commentid`
+            FOREIGN KEY (CommentID) REFERENCES Comments (CommentID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
 
-        #Fetch PlayerStatsID
-        cursor.execute("""SELECT PlayerStatsID FROM PlayerStats
-        WHERE GameID = {} AND SteamID = {}""".format(LogID,PlayerID))
-        PlayerStatsID = cursor.fetchone()[0]
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS PollOptions (
+        PollOptionID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        PollID INT UNSIGNED NOT NULL,
+        Option VARCHAR(300),
+        PRIMARY KEY (PollOptionID),
+        CONSTRAINT `fk_polloptions_pollid`
+            FOREIGN KEY (PollID) REFERENCES Polls (PollID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
 
-        #Add class stats for player
-        for Class_stats in PlayerStats["class_stats"]:
-            if Class_stats["type"] not in CLASS_ID:
-                continue
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS PollVotes (
+        UserID INT UNSIGNED NOT NULL,
+        PollID INT UNSIGNED NOT NULL,
+        PollOptionID INT UNSIGNED NOT NULL,
+        PRIMARY KEY (UserID, PollID),
+        CONSTRAINT `fk_user_pollvotes`
+            FOREIGN KEY (UserID) REFERENCES Users (UserID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+        CONSTRAINT `fk_poll_pollvotes`
+            FOREIGN KEY (PollID) REFERENCES Polls (PollID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+        CONSTRAINT `fk_polloption_pollvotes`
+            FOREIGN KEY (PollOptionID) REFERENCES PollOptions (PollOptionID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
 
-            Class       = CLASS_ID[Class_stats["type"]]
-            Kills       = Class_stats["kills"]
-            Assists     = Class_stats["assists"]
-            Deaths      = Class_stats["deaths"]
-            Damage      = Class_stats["dmg"]
-            Duration    = Class_stats["total_time"]
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Games (
+        GameID INT UNSIGNED NOT NULL,
+        Date INT UNSIGNED,
+        Duration SMALLINT,
+        Map VARCHAR(32),
+        BluScore TINYINT UNSIGNED,
+        RedScore TINYINT UNSIGNED,
+        PRIMARY KEY (GameID)
+    ) ENGINE = InnoDB;
+    """)
 
-            if Duration < 0:
-                continue
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Players (
+        SteamID BIGINT UNSIGNED NOT NULL,
+        PlayerName VARCHAR(32),
+        PRIMARY KEY (SteamID)
+    ) ENGINE = InnoDB CHARACTER SET 'utf8mb4';
+    """)
 
-            if (Class == 7) and (Duration > 0) and ((Heals/Duration)*60 > HPM_THRESH):
-                BlackListLog(LogID,"heals thresh exceeded", cursor)
-                return
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS PlayerStats (
+        PlayerStatsID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        GameID INT UNSIGNED NOT NULL,
+        SteamID BIGINT UNSIGNED NOT NULL,
+        TeamID TINYINT UNSIGNED NOT NULL,
+        DamageTaken MEDIUMINT UNSIGNED,
+        HealsReceived MEDIUMINT UNSIGNED,
+        MedkitsHP MEDIUMINT UNSIGNED,
+        Airshots TINYINT UNSIGNED,
+        Headshots TINYINT UNSIGNED,
+        Backstabs TINYINT UNSIGNED,
+        Drops TINYINT UNSIGNED,
+        Heals MEDIUMINT UNSIGNED,
+        Ubers TINYINT UNSIGNED,
+        PRIMARY KEY (PlayerStatsID),
+        CONSTRAINT `fk_game_id`
+            FOREIGN KEY (GameID) REFERENCES Games (GameID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+        CONSTRAINT `fk_player_id`
+            FOREIGN KEY (SteamID) REFERENCES Players (SteamID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB;
+    """)
 
-            #Add record
-            cursor.execute("""INSERT INTO ClassStats
-            (PlayerStatsID, ClassID, Playtime, Kills, Assists, Deaths, Damage)
-            VALUES ({},{},{},{},{},{},{})""".format(
-            PlayerStatsID, Class,Duration,Kills,Assists,Deaths,Damage))
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS ClassStats (
+        ClassStatsID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        PlayerStatsID INT UNSIGNED NOT NULL,
+        ClassID TINYINT UNSIGNED NOT NULL,
+        Playtime SMALLINT UNSIGNED,
+        Kills TINYINT UNSIGNED,
+        Assists TINYINT UNSIGNED,
+        Deaths TINYINT UNSIGNED,
+        Damage MEDIUMINT UNSIGNED,
+        PRIMARY KEY (ClassStatsID),
+        CONSTRAINT `fk_playerstats_id`
+            FOREIGN KEY (PlayerStatsID) REFERENCES PlayerStats (PlayerStatsID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    ) ENGINE = InnoDB;
+    """)
 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Weapons (
+        WeaponID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        WeaponName VARCHAR(32),
+        PRIMARY KEY (WeaponID)
+    ) ENGINE = InnoDB;
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS WeaponStats (
+        PlayerStatsID INT UNSIGNED NOT NULL,
+        WeaponID SMALLINT UNSIGNED NOT NULL,
+        Accuracy DOUBLE,
+        PRIMARY KEY (PlayerStatsID, WeaponID),
+        CONSTRAINT `fk_player_stats`
+            FOREIGN KEY (PlayerStatsID) REFERENCES PlayerStats (PlayerStatsID)
+            ON DELETE CASCADE
+            ON UPDATE RESTRICT,
+        CONSTRAINT `fk_weapon_id`
+            FOREIGN KEY (WeaponID) REFERENCES Weapons (WeaponID)
+            ON DELETE CASCADE
+            ON UPDATE RESTRICT
+    ) ENGINE = InnoDB;
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS BlacklistGames (
+        GameID INT UNSIGNED NOT NULL,
+        Reason VARCHAR(32),
+        PRIMARY KEY (GameID)
+    ) ENGINE = InnoDB;
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Progress(
+        ProgressID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        EndDate INT UNSIGNED,
+        SteamID BIGINT UNSIGNED,
+        Matches INT,
+        Hours DOUBLE,
+        Kills DOUBLE,
+        Deaths DOUBLE,
+        Assists DOUBLE,
+        Backstabs DOUBLE,
+        Headshots DOUBLE,
+        Airshots DOUBLE,
+        DPM DOUBLE,
+        DTM DOUBLE,
+        HRM DOUBLE,
+        PRIMARY KEY (ProgressID),
+        CONSTRAINT `fk_progress_steamid`
+            FOREIGN KEY (SteamID) REFERENCES Players (SteamID)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    )ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+    """)
     print("done")
 
-def deSmurf(cursors, originals, smurfs):
-    print('clearing smurfs...', end="")
-    for i, v in enumerate(originals):
-        cursor.execute("UPDATE PlayerStats SET SteamID={} WHERE SteamID={}".format(originals[i],smurfs[i]))
     return
-    print('done')
-
-if __name__ == "__main__":
-    env = dotenv_values("../.env")
-    db = mysql.connector.connect(
-        host        = "localhost",
-        user        = env['MYSQL_USR'],
-        password    = env['MYSQL_PWD'],
-        database    = env['MYSQL_DB'],
-    )
-    cursor = db.cursor()
-    cursor.autocommit = True
-    #Fetch command line args
-    LIMIT = -1
-    if len(sys.argv) > 1:
-        LIMIT = int(sys.argv[1])
-        if "stat" in sys.argv:
-            cursor.execute("DROP TABLE IF EXISTS WeaponStats, Weapons, ClassStats, PlayerStats, Players, Games, BlacklistGames;")
-        if "forum" in sys.argv:
-            cursor.execute("DROP TABLE IF EXISTS PollVotes, PollOptions, Polls, Images, Comments, Threads, Users")
-
-    DBInit(cursor)
-
-    UpdatePlayerNames(cursor)
-
-    #get blacklisted logs
-    cursor.execute("SELECT GameID FROM BlacklistGames")
-    BlackListedLogs = [i[0] for i in list(cursor.fetchall())]
-    print(str(len(BlackListedLogs)) + " blacklisted matches have been recorded.")
-
-    #get already stored logs
-    cursor.execute("SELECT GameID FROM Games")
-    StoredLogs = [i[0] for i in list(cursor.fetchall())]
-    print(len(StoredLogs), " matches already stored in database.\nFetching Logs...", end="")
-
-    UPLOADERS = env['LOG_UPLOADERS'].split(',')
-    AllLogs = []
-    #Generate list of LogIDs
-    UpdatePlayerNames(cursor)
-    for x in UPLOADERS:
-        UploaderLogs = json.loads(requests.get("https://logs.tf/api/v1/log?uploader={}".format(x)).text)
-        for x in UploaderLogs["logs"]:
-            AllLogs.append(x["id"])
-
-    print(len(AllLogs), "Total logs")
-
-    LogsList = set(set(AllLogs).difference(StoredLogs)).difference(BlackListedLogs)
-    print(len(LogsList),  "logs to be processed")
-
-    #Add each Log record
-    for idx, LogID in enumerate(LogsList, start=1):
-        print("Updating log (" + str(idx) + "/" + str(len(LogsList)) + ") - " + str(LogID) + '... ', end='')
-        Log = json.loads(requests.get("http://logs.tf/api/v1/log/{}".format(LogID)).text)
-        AddGame(LogID, Log, cursor)
-        db.commit()
-        if idx == LIMIT:
-            break
-
-    deSmurf(cursor, env['ORIGINALS'].split(','), env['SMURFS'].split(','))
-    updateProgress(cursor)
-
-    UpdatePlayerNames(cursor)
-    db.commit()
-    db.close()
