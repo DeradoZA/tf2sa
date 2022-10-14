e975ced89842f8d9ee5d2e6dc6ea37245b98ecf8
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-14 20:27:01 +0200

    add di

---
+++ dotnet/TF2SA.Http/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using TF2SA.Http.LogsTF.LogsTFHttpClient;
+
+namespace TF2SA.Http
+{
+    public static class DependencyInjection
+    {
+        public static void AddHttpServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddHttpClient();
+			services.AddTransient<ILogsTFHttpClient, LogsTFHttpClient>();
+		}
+    }
+}
