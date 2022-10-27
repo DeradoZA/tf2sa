d2c62413b77ef605abe8164f83d5d00eef566b54
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-27 20:51:48 +0200

    update tests to use monad definition

--- dotnet/TF2SA.Tests.Unit/Http/SerializationTests.cs
+++ dotnet/TF2SA.Tests.Unit/Http/SerializationTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Monad;
+using TF2SA.Http.Errors;
 using TF2SA.Http.LogsTF.Models.GameLogModel;
 using TF2SA.Http.LogsTF.Serialization;
 using Xunit;
@@ -10,12 +12,17 @@ namespace TF2SA.Tests.Unit.Http;
 
 public class SerializationTests
 {
-    [Fact]
-    public void TestRootGameLog()
-    {
-        GameLog gameLog = LogsTFSerializer.DeserializeGameLog(SerializationStubs.NormalGameLogJsonResponse);
-        uint expected = 1787;
-        Assert.Equal(gameLog?.Length, expected);
-        Assert.Equal(gameLog?.Players?["[U:1:146905179]"].Team, "Red");
-    }
+	[Fact]
+	public void TestRootGameLog()
+	{
+		EitherStrict<SerializationError, GameLog> gameLog =
+			LogsTFSerializer<GameLog>
+			.Deserialize(SerializationStubs.NormalGameLogJsonResponse);
+		Assert.True(gameLog.IsRight);
+
+		Assert.Equal(1787U, gameLog.Right.Length);
+		Assert.Equal("Blue", gameLog.Right.Players?["[U:1:146905179]"].Team);
+		Assert.True(gameLog.Right.Success);
+	}
+
 }
