34f33578bfd3e3c8b69c751f7197820f5379c08f
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-27 20:09:09 +0200

    generify deserialize method

--- dotnet/TF2SA.Http/LogsTF/Serialization/LogsTFSerializer.cs
+++ dotnet/TF2SA.Http/LogsTF/Serialization/LogsTFSerializer.cs
@@ -1,15 +1,28 @@
 using System.Text.Json;
+using Monad;
+using TF2SA.Http.Errors;
 using TF2SA.Http.LogsTF.Models.GameLogModel;
 
 namespace TF2SA.Http.LogsTF.Serialization;
 
-public static class LogsTFSerializer
+public static class LogsTFSerializer<T>
 {
-    public static GameLog? DeserializeGameLog(string json) // use monads here.
+    public static EitherStrict<SerializationError, T> Deserialize(string json)
     {
         JsonSerializerOptions jsonOptions =
             new() { PropertyNameCaseInsensitive = true };
-        var serialized = JsonSerializer.Deserialize<GameLog>(json, jsonOptions);
-        return serialized;
+        try
+        {
+            T? serialized = JsonSerializer.Deserialize<T>(json, jsonOptions);
+            if (serialized is null)
+            {
+                return new SerializationError("Serialized result is null");
+            }
+            return serialized;
+        }
+        catch (Exception e)
+        {
+            return new SerializationError(e.Message);
+        }
     }
 }
