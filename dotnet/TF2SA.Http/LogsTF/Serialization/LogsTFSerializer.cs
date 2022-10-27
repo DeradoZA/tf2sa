94e8204ba0f4ddca14bda0c6edf107441070b79a
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-27 23:56:32 +0200

    adjust class member

--- dotnet/TF2SA.Http/LogsTF/Serialization/LogsTFSerializer.cs
+++ dotnet/TF2SA.Http/LogsTF/Serialization/LogsTFSerializer.cs
@@ -7,22 +7,22 @@ namespace TF2SA.Http.LogsTF.Serialization;
 
 public static class LogsTFSerializer<T>
 {
-    public static EitherStrict<SerializationError, T> Deserialize(string json)
-    {
-        JsonSerializerOptions jsonOptions =
-            new() { PropertyNameCaseInsensitive = true };
-        try
-        {
-            T? serialized = JsonSerializer.Deserialize<T>(json, jsonOptions);
-            if (serialized is null)
-            {
-                return new SerializationError("Serialized result is null");
-            }
-            return serialized;
-        }
-        catch (Exception e)
-        {
-            return new SerializationError(e.Message);
-        }
-    }
+	private static readonly JsonSerializerOptions jsonOptions =
+		new() { PropertyNameCaseInsensitive = true };
+	public static EitherStrict<SerializationError, T> Deserialize(string json)
+	{
+		try
+		{
+			T? serialized = JsonSerializer.Deserialize<T>(json, jsonOptions);
+			if (serialized is null)
+			{
+				return new SerializationError("Serialized result is null");
+			}
+			return serialized;
+		}
+		catch (Exception e)
+		{
+			return new SerializationError(e.Message);
+		}
+	}
 }
