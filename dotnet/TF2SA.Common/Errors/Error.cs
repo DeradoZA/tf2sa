c3c78b2f8ccf057ceb18ba4e7727b1ee64271126
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-27 19:31:27 +0200

    formatting

--- dotnet/TF2SA.Common/Errors/Error.cs
+++ dotnet/TF2SA.Common/Errors/Error.cs
@@ -5,30 +5,27 @@ using System.Threading.Tasks;
 
 namespace TF2SA.Common.Errors;
 
-public class Error
+public abstract class Error
 {
-    public abstract class Error
+    public string Message { get; set; } = string.Empty;
+
+    public virtual Exception AsException()
     {
-        public string Message { get; set; }
+        return new Exception(Message);
+    }
 
-        public virtual Exception AsException()
-        {
-            return new Exception(Message);
-        }
+    public virtual Exception AsException(Exception innerException)
+    {
+        return new Exception(Message, innerException);
+    }
 
-        public virtual Exception AsException(Exception innerException)
-        {
-            return new Exception(Message, innerException);
-        }
+    public void Throw()
+    {
+        throw AsException();
+    }
 
-        public void Throw()
-        {
-            throw AsException();
-        }
-
-        public void Throw(Exception innerException)
-        {
-            throw AsException(innerException);
-        }
+    public void Throw(Exception innerException)
+    {
+        throw AsException(innerException);
     }
 }
