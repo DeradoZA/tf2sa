ef171fe74576821e880255fe706ea8fd0db9aa3f
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-27 18:59:53 +0200

    add generic error class and common project

---
+++ dotnet/TF2SA.Common/Errors/Error.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TF2SA.Common.Errors
+{
+    public class Error
+    {
+        public abstract class Error
+        {
+            public string Message { get; set; }
+
+            public virtual Exception AsException()
+            {
+                return new Exception(Message);
+            }
+
+            public virtual Exception AsException(Exception innerException)
+            {
+                return new Exception(Message, innerException);
+            }
+
+            public void Throw()
+            {
+                throw AsException();
+            }
+
+            public void Throw(Exception innerException)
+            {
+                throw AsException(innerException);
+            }
+        }
+    }
+}
