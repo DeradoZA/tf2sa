5068909eafa7581d13d02d5e7aa75680734bdc4d
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-27 23:56:48 +0200

    remove unused imports

--- dotnet/TF2SA.Tests.Unit/Http/SerializationTests.cs
+++ dotnet/TF2SA.Tests.Unit/Http/SerializationTests.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Monad;
-using TF2SA.Http.Errors;
 using TF2SA.Http.LogsTF.Models.GameLogModel;
 using TF2SA.Http.LogsTF.Serialization;
 using Xunit;
