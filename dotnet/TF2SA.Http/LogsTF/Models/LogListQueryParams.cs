528cc12d0c685950dccc059fa0bb6c5012a6f25a
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-10-30 12:34:37 +0200

    adjust queryparams

--- dotnet/TF2SA.Http/LogsTF/Models/LogListQueryParams.cs
+++ dotnet/TF2SA.Http/LogsTF/Models/LogListQueryParams.cs
@@ -1,12 +1,11 @@
-namespace TF2SA.Http.LogsTF.Models
+namespace TF2SA.Http.LogsTF.Models;
+
+public class LogListQueryParams
 {
-	public class LogListQueryParams
-	{
-		public string Title { get; set; } = string.Empty;
-		public string Map { get; set; } = string.Empty;
-		public uint Uploader { get; set; }
-		public uint[] Players { get; set; } = Array.Empty<uint>();
-		public int Limit { get; set; } = -1;
-		public int Offset { get; set; } = -1;
-	}
+	public string Title { get; set; } = string.Empty;
+	public string Map { get; set; } = string.Empty;
+	public uint Uploader { get; set; }
+	public uint[] Players { get; set; } = Array.Empty<uint>();
+	public int Limit { get; set; }
+	public int Offset { get; set; }
 }
