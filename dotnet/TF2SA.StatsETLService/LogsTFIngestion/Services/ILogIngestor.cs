8cf4ee59ccf57c83e0252a26f70eae998803e347
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-12-23 16:32:07 +0200

    define log ingestor interface

--- dotnet/TF2SA.StatsETLService/LogsTFIngestion/Services/ILogIngestor.cs
+++ dotnet/TF2SA.StatsETLService/LogsTFIngestion/Services/ILogIngestor.cs
@@ -1,3 +1,14 @@
+using Monad;
+using TF2SA.Common.Errors;
+using TF2SA.Common.Models.LogsTF.LogListModel;
+using TF2SA.StatsETLService.LogsTFIngestion.Errors;
+
 namespace TF2SA.StatsETLService.LogsTFIngestion.Services;
 
-public interface ILogIngestor { }
+public interface ILogIngestor
+{
+	Task<OptionStrict<List<Error>>> IngestLog(
+		LogListItem logListItem,
+		CancellationToken cancellationToken
+	);
+}
