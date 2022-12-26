a71dc7cadd242e5516910eb7d2a88b8d5b57c927
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-12-26 16:17:15 +0200

    add conditions

--- dotnet/TF2SA.StatsETLService/LogsTFIngestion/Validation/GameLogValidator.cs
+++ dotnet/TF2SA.StatsETLService/LogsTFIngestion/Validation/GameLogValidator.cs
@@ -8,8 +8,6 @@ public class GameLogValidator : AbstractValidator<GameLog>
 	public const uint MIN_GAME_LENGTH = 10 * 60;
 	public const int MIN_PLAYERS = 10;
 	public const int MAX_PLAYERS = 15;
-	public const int MAX_DAMAGE = 15;
-	public const int MAX_HEALS_PER_MIN = 1400;
 
 	public GameLogValidator()
 	{
