9b936a6672b868260875ac6d6da7f14388aee8f1
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-12-28 20:00:01 +0200

    add tests to check player count

---
+++ dotnet/TF2SA.Tests.Unit/StatsETLService/Validation/GameLogValidatorTests.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using FluentValidation.Results;
+using TF2SA.Common.Models.LogsTF.GameLogModel;
+using TF2SA.StatsETLService.LogsTFIngestion.Validation;
+using Xunit;
+
+namespace TF2SA.Tests.Unit.StatsETLService.Validation;
+
+public class GameLogValidatorTests
+{
+	private readonly GameLogValidator validator = new();
+	private readonly Fixture fixture = new();
+
+	[Fact]
+	public void GivenNullRootFields_ValidationFails_WithExpectedErrors()
+	{
+		GameLog gamelog = new();
+		List<string> expectedErrors =
+			new()
+			{
+				"'Length' must not be empty.",
+				"'Players' must not be empty."
+			};
+
+		ValidationResult result = validator.Validate(gamelog);
+
+		Assert.False(result.IsValid);
+		Assert.All(result.Errors, e => expectedErrors.Contains(e.ErrorMessage));
+	}
+
+	[Theory]
+	[InlineData(8)]
+	[InlineData(9)]
+	[InlineData(17)]
+	[InlineData(18)]
+	public void GivenNon6v6AmountOfPlayers_ValidationFails_WithExpectedErrors(
+		int playerCount
+	)
+	{
+		GameLog gamelog = CreateValidGameLog(playerCount);
+
+		string expectedError =
+			"Game is likely not a 6v6 game. Must contain 10-16 players.";
+
+		ValidationResult result = validator.Validate(gamelog);
+
+		Assert.False(result.IsValid);
+		Assert.Contains(
+			result.Errors,
+			e =>
+				string.Equals(
+					expectedError,
+					e.ErrorMessage,
+					StringComparison.InvariantCultureIgnoreCase
+				)
+		);
+	}
+
+	[Theory]
+	[InlineData(10)]
+	[InlineData(12)]
+	[InlineData(16)]
+	public void Given6v6AmountOfPlayers_ValidationPasses(int playerCount)
+	{
+		GameLog gamelog = CreateValidGameLog(playerCount);
+
+		ValidationResult result = validator.Validate(gamelog);
+
+		Assert.True(result.IsValid);
+	}
+
+	private GameLog CreateValidGameLog(int playerCount)
+	{
+		GameLog gamelog = fixture
+			.Build<GameLog>()
+			.With(x => x.Length, 1000U)
+			.Create();
+		gamelog.Players = fixture
+			.Build<PlayerStats>()
+			.With(p => p.Heals, 0)
+			.With(p => p.Damage, 5000)
+			.CreateMany(playerCount)
+			.ToList();
+		return gamelog;
+	}
+}
