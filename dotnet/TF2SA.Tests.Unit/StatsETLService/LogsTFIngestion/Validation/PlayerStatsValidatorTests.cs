6c1624e022762a4c26581a52d8df37eed3ea6a3a
Adam Coetzee <adam.coetzee@angloamerican.com>
2022-12-28 21:46:31 +0200

    add remaining playerstats validation

--- dotnet/TF2SA.Tests.Unit/StatsETLService/LogsTFIngestion/Validation/PlayerStatsValidatorTests.cs
+++ dotnet/TF2SA.Tests.Unit/StatsETLService/LogsTFIngestion/Validation/PlayerStatsValidatorTests.cs
@@ -35,4 +35,80 @@ public class PlayerStatsValidatorTests
 		Assert.False(result.IsValid);
 		Assert.All(expectedErrors, e => Assert.Contains(e, returnedErrors));
 	}
+
+	[Fact]
+	public void GivenInvalidDamage_ValidationFails_WithExpectedError()
+	{
+		PlayerStats playerStats = fixture
+			.Build<PlayerStats>()
+			.With(p => p.Damage, 18000)
+			.With(p => p.Heals, 0)
+			.Create();
+		string expectedError =
+			"'Damage' must be less than or equal to '17500'.";
+
+		ValidationResult result = validator.Validate(playerStats);
+		IEnumerable<string> returnedErrors = result.Errors.Select(
+			re => re.ErrorMessage
+		);
+
+		Assert.False(result.IsValid);
+		Assert.Contains(expectedError, returnedErrors);
+	}
+
+	[Fact]
+	public void GivenValidDamage_ValidationPasses()
+	{
+		PlayerStats playerStats = fixture
+			.Build<PlayerStats>()
+			.With(p => p.Damage, 6000)
+			.With(p => p.Heals, 0)
+			.Create();
+
+		ValidationResult result = validator.Validate(playerStats);
+
+		Assert.True(result.IsValid);
+	}
+
+	[Fact]
+	public void GivenInvalidHealsPerMinute_ValidationFails_WithExpectedError()
+	{
+		PlayerStats playerStats = GenerateMedicClassStat(2000, 60);
+		string expectedError =
+			"Player played medic with abnormally high heals per minute";
+
+		ValidationResult result = validator.Validate(playerStats);
+		IEnumerable<string> returnedErrors = result.Errors.Select(
+			re => re.ErrorMessage
+		);
+
+		Assert.False(result.IsValid);
+		Assert.Contains(expectedError, returnedErrors);
+	}
+
+	[Fact]
+	public void InvalidHealsPerMinute_ValidationPassses()
+	{
+		PlayerStats playerStats = GenerateMedicClassStat(1000, 60);
+
+		ValidationResult result = validator.Validate(playerStats);
+
+		Assert.True(result.IsValid);
+	}
+
+	private PlayerStats GenerateMedicClassStat(int heals, int medicPlayTime) =>
+		fixture
+			.Build<PlayerStats>()
+			.With(p => p.Damage, 600)
+			.With(p => p.Heals, heals)
+			.With(
+				p => p.ClassStats,
+				fixture
+					.Build<ClassStats>()
+					.With(c => c.TotalTime, medicPlayTime)
+					.With(c => c.Type, "medic")
+					.CreateMany(1)
+					.ToList()
+			)
+			.Create();
 }
