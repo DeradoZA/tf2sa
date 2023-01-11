dfc3673cba209b1620a9c443121efe51643b08b4
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-01-11 11:34:26 +0200

    closes #109 nullability adjustments to cater for dirty data

--- dotnet/TF2SA.StatsETLService/LogsTFIngestion/Validation/PlayerStatsValidator.cs
+++ dotnet/TF2SA.StatsETLService/LogsTFIngestion/Validation/PlayerStatsValidator.cs
@@ -22,10 +22,8 @@ public class PlayerStatsValidator : AbstractValidator<PlayerStats>
 
 		RuleForEach(p => p.ClassStats).SetValidator(new ClassStatsValidator());
 
-		RuleFor(p => p.DamageTaken).NotNull();
 		RuleFor(p => p.LongestKillStreak).NotNull();
 		RuleFor(p => p.MedKits).NotNull();
-		RuleFor(p => p.MedkitsHp).NotNull();
 
 		RuleFor(p => p.Damage)
 			.NotNull()
