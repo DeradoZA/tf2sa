29afe3cf616199fbda6adde82fdc10120053da65
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-01-15 22:21:13 +0200

    add home page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, HomeComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
