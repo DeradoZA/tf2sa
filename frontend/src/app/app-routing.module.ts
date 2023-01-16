6fc290c71fedfc8ab83e96364db59beed6e6453a
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-01-16 09:41:50 +0200

    define routes with variables

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { PlayersComponent } from './components/pages/players/players.component';
 import { StatsComponent } from './components/pages/stats/stats.component';
+import { ROUTES } from './routes';
 
 const routes: Routes = [
-	{ path: HomeComponent.PATH, component: HomeComponent },
-	{ path: StatsComponent.PATH, component: StatsComponent },
-	{ path: PlayersComponent.PATH, component: PlayersComponent },
+	{ path: ROUTES.HOME, component: HomeComponent },
+	{ path: ROUTES.STATS, component: StatsComponent },
+	{ path: ROUTES.PLAYERS, component: PlayersComponent },
 	{ path: '', redirectTo: HomeComponent.PATH, pathMatch: 'full' },
 	{ path: '**', redirectTo: HomeComponent.PATH },
 ];
---
+++ frontend/src/app/routes.ts
@@ -0,0 +1,9 @@
+import { HomeComponent } from './components/pages/home/home.component';
+import { PlayersComponent } from './components/pages/players/players.component';
+import { StatsComponent } from './components/pages/stats/stats.component';
+
+export const ROUTES = {
+	HOME: HomeComponent.PATH,
+	STATS: StatsComponent.PATH,
+	PLAYERS: PlayersComponent.PATH,
+};
