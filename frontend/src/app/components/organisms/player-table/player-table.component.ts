3c3792476bae3e74093620546d106ec9616cb429
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-01-23 17:29:24 +0200

    move data fetching logic to table

--- frontend/src/app/components/organisms/player-table/player-table.component.ts
+++ frontend/src/app/components/organisms/player-table/player-table.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
-import { Player } from 'src/app/models/player';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { GetPlayersResult } from 'src/app/services/players/getPlayersResult';
+import { PlayersService } from 'src/app/services/players/players.service';
 
 @Component({
 	selector: 'app-player-table',
 	templateUrl: './player-table.component.html',
 	styleUrls: ['./player-table.component.scss'],
 })
-export class PlayerTableComponent {
-	@Input() players!: Player[];
+export class PlayerTableComponent implements OnInit, OnDestroy {
+	constructor(private playersService: PlayersService) {}
+	isLoaded: boolean = false;
+	subscription: Subscription | undefined;
+	playersResult: GetPlayersResult | undefined;
+	errorMessage: string | undefined;
+
+	ngOnInit(): void {
+		this.subscription = this.playersService.getPlayers().subscribe({
+			next: (players) => {
+				console.log(players);
+				this.playersResult = players;
+				this.isLoaded = true;
+			},
+			error: (error) => {
+				this.errorMessage = error;
+				this.isLoaded = true;
+			},
+		});
+	}
+	ngOnDestroy(): void {
+		this.subscription?.unsubscribe();
+	}
+
 	displayedColumns: string[] = ['name', 'steamId'];
 }
