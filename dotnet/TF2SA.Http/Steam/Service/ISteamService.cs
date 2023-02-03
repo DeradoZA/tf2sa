2acd0f0e3802fc0e2bea598d82e63335bcb00b80
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-03 09:58:32 +0200

    adjust interface def

--- dotnet/TF2SA.Http/Steam/Service/ISteamService.cs
+++ dotnet/TF2SA.Http/Steam/Service/ISteamService.cs
@@ -1,12 +1,12 @@
 using Monad;
-using TF2SA.Http.Base.Errors;
+using TF2SA.Common.Errors;
 using TF2SA.Http.Steam.Config.Models.PlayerSummaries;
 
 namespace TF2SA.Http.Steam.Service;
 
 public interface ISteamService
 {
-	public Task<EitherStrict<HttpError, SteamPlayersResponse>> GetPlayers(
+	public Task<EitherStrict<Error, List<SteamPlayer>>> GetPlayers(
 		ulong[] steamids
 	);
 }
