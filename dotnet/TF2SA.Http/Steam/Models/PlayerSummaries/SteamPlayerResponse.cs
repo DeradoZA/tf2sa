ce9cc203c094b31d1137bdc26f628fe8d3ffa6f2
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-07 15:10:37 +0200

    add nullability to models

--- dotnet/TF2SA.Http/Steam/Models/PlayerSummaries/SteamPlayerResponse.cs
+++ dotnet/TF2SA.Http/Steam/Models/PlayerSummaries/SteamPlayerResponse.cs
@@ -5,5 +5,5 @@ namespace TF2SA.Http.Steam.Models.PlayerSummaries;
 public class SteamPlayerResponse
 {
 	[JsonPropertyName("players")]
-	public List<SteamPlayer> Players { get; set; }
+	public List<SteamPlayer>? Players { get; set; }
 }
--- dotnet/TF2SA.Http/Steam/Models/PlayerSummaries/SteamPlayerResponseRoot.cs
+++ dotnet/TF2SA.Http/Steam/Models/PlayerSummaries/SteamPlayerResponseRoot.cs
@@ -5,5 +5,5 @@ namespace TF2SA.Http.Steam.Models.PlayerSummaries;
 public class SteamPlayerResponseRoot
 {
 	[JsonPropertyName("response")]
-	public SteamPlayerResponse Response { get; set; }
+	public SteamPlayerResponse? Response { get; set; }
 }
