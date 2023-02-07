d4b9b30def01ec6755ac46b15d4bd96101b502ea
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-07 12:31:51 +0200

    correct configuration binding to di

--- dotnet/TF2SA.Http/DependencyInjection.cs
+++ dotnet/TF2SA.Http/DependencyInjection.cs
@@ -21,10 +21,10 @@ public static class DependencyInjection
 		);
 		services.Configure<SteamConfig>(config =>
 		{
+			configuration.Bind(SteamConfig.SteamConfigSection, config);
 			config.ApiKey = configuration.GetValue<string>(
 				"TF2SA_STEAM_API_KEY"
 			);
-			configuration.Bind(SteamConfig.SteamConfigSection, config);
 		});
 		services.AddHttpClient();
 		services.AddTransient<IHttpClient, TF2SAHttpClient>();
