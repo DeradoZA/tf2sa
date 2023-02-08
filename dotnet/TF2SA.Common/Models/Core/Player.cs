b2122d23a94fbbda36a758a16e04a3f7828659e5
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-08 08:37:25 +0200

    add properties to models

--- dotnet/TF2SA.Common/Models/Core/Player.cs
+++ dotnet/TF2SA.Common/Models/Core/Player.cs
@@ -4,4 +4,11 @@ public class Player
 {
 	public ulong SteamId { get; set; }
 	public string? PlayerName { get; set; }
+	public string? LocalCountryCode { get; set; }
+	public string? ProfileUrl { get; set; }
+	public string? Avatar { get; set; }
+	public string? AvatarMedium { get; set; }
+	public string? AvatarFull { get; set; }
+	public string? AvatarHash { get; set; }
+	public string? RealName { get; set; }
 }
