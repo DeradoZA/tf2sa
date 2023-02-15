43f55dac1f78a00261844b82b952ea6b983b4c3c
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-15 16:05:51 +0200

    scaffold stat service

--- frontend/src/app/services/stats/stats.service.ts
+++ frontend/src/app/services/stats/stats.service.ts
@@ -7,10 +7,39 @@ import {
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { DEFAULT_PAGE_SIZE } from '../httpConstants';
+
+const STATS_BASE_URL = `${environment.backendUrl}/Statistics`;
 
 @Injectable({
 	providedIn: 'root',
 })
 export class StatsService {
 	constructor(private httpClient: HttpClient) {}
+
+	getPlayers(
+		count = DEFAULT_PAGE_SIZE,
+		offset = 0,
+		sort = 'playerName',
+		sortorder = 'asc',
+		filterString: string
+	) {
+		let params = new HttpParams();
+		params = params.append('count', count.toString());
+		params = params.append('offset', offset.toString());
+		params = params.append('sort', sort);
+		params = params.append('sortorder', sortorder);
+		params = params.append('filterString', filterString);
+
+		return (
+			this.httpClient
+				// type
+				.get(`${STATS_BASE_URL}/ScoutRecent`, { params: params })
+				.pipe(catchError(this.handleError))
+		);
+	}
+
+	private handleError(error: HttpErrorResponse) {
+		return throwError(() => new Error(error.statusText));
+	}
 }
