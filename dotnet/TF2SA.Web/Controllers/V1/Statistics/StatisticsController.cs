30ec9f9307e92ab4fc3626300c3c0d50e7bfc81e
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-20 17:13:45 +0200

    split controller into partial for each class

--- dotnet/TF2SA.Web/Controllers/V1/Statistics/StatisticsController.cs
+++ dotnet/TF2SA.Web/Controllers/V1/Statistics/StatisticsController.cs
@@ -11,7 +11,7 @@ namespace TF2SA.Web.Controllers.V1.Statistics;
 
 [ApiController]
 [Route("v1/[controller]")]
-public class StatisticsController : ControllerBase
+public partial class StatisticsController : ControllerBase
 {
 	private readonly IMediator mediator;
 	private readonly IMapper mapper;
@@ -26,40 +26,5 @@ public class StatisticsController : ControllerBase
 		this.mediator = mediator;
 		this.mapper = mapper;
 		this.logger = logger;
-	}
-
-	// TODO split into partial classes
-	// milestone: 8
-	[HttpGet]
-	[Route("ScoutRecent")]
-	public async Task<ActionResult<GetScoutStatsHttpResult>> GetScoutRecent(
-		[FromQuery] int count = 13,
-		[FromQuery] int offset = 0,
-		[FromQuery] string? sort = "averageDpm",
-		[FromQuery] string? sortOrder = "desc",
-		[FromQuery] string? filterField = "",
-		[FromQuery] string? filterValue = ""
-	)
-	{
-		EitherStrict<Error, GetScoutStatsResult> result = await mediator.Send(
-			new GetScoutRecentQuery(
-				count,
-				offset,
-				sort!,
-				sortOrder!,
-				filterField!,
-				filterValue!
-			)
-		);
-
-		if (result.IsLeft)
-		{
-			return BadRequest(result.Left.Message);
-		}
-
-		GetScoutStatsHttpResult httpResult =
-			mapper.Map<GetScoutStatsHttpResult>(result.Right);
-
-		return Ok(httpResult);
 	}
 }
