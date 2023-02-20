de48d235e78577905eb86425182fb33209ae584f
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-20 15:03:16 +0200

    add additional values to scout table

--- frontend/src/app/services/stats/getScoutStats.ts
+++ frontend/src/app/services/stats/getScoutStats.ts
@@ -7,10 +7,17 @@ export interface ScoutStat {
 	playerName: string;
 	avatar: string;
 	numberOfGames: number;
+	wins: number;
+	draws: number;
+	losses: number;
 	averageDpm: number;
 	averageKills: number;
 	averageAssists: number;
 	averageDeaths: number;
+	averageDamageTakenPm: number;
+	averageHealsReceivedPm: number;
+	averageMedKitsHp: number;
+	averageCapturePointsCaptured: number;
 	topKills: number;
 	topKillsGameId: number;
 	topDamage: number;
