6ee9264e2bcae6b7a6e54b159b642be469cb327a
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-21 10:30:15 +0200

    add soldier and scout stat repositories

---
+++ dotnet/TF2SA.Data/Repositories/MariaDb/Statistics/SoldierAllTimeStatsRepository.cs
@@ -0,0 +1,57 @@
+using System.Linq.Expressions;
+using Microsoft.Extensions.Logging;
+using TF2SA.Data.Entities.MariaDb;
+using static TF2SA.Data.Extensions.TF2SALinqExtensions;
+
+namespace TF2SA.Data.Repositories.MariaDb.Statistics;
+
+public class SoldierAllTimeStatsRepository : StatsRepository<SoldierAllTime>
+{
+	public override string UpdateProcQuery =>
+		"CALL UpdateSoldierAllTimeStats();";
+
+	public override Dictionary<
+		string,
+		Expression<Func<SoldierAllTime, object>>
+	> PropertyKeySelectors =>
+		new()
+		{
+			{ "steamId", s => s.SteamId },
+			{ "playerName", s => s.PlayerName! },
+			{ "avatar", s => s.Avatar! },
+			{ "numberOfGames", s => s.NumberOfGames! },
+			{ "wins", s => s.Wins! },
+			{ "draws", s => s.Draws! },
+			{ "losses", s => s.Losses! },
+			{ "averageDpm", s => s.AverageDpm! },
+			{ "averageKills", s => s.AverageKills! },
+			{ "averageAssists", s => s.AverageAssists! },
+			{ "averageDeaths", s => s.AverageDeaths! },
+			{ "averageAirshots", s => s.AverageAirshots! },
+			{ "averageDamageTakenPm", s => s.AverageDamageTakenPm! },
+			{ "averageHealsReceivedPm", s => s.AverageHealsReceivedPm! },
+			{ "averageMedKitsHp", s => s.AverageMedKitsHp! },
+			{
+				"averageCapturePointsCaptured",
+				s => s.AverageCapturePointsCaptured!
+			},
+			{ "topKills", s => s.TopKills! },
+			{ "topKillsGameId", s => s.TopKillsGameId! },
+			{ "topDamage", s => s.TopDamage! },
+			{ "topDamageGameId", s => s.TopDamageGameId! },
+			{ "topAirshots", s => s.TopAirshots! },
+			{ "topAirshotsGameId", s => s.TopAirshotsGameId! },
+		};
+
+	public override Tuple<
+		string,
+		Expression<Func<SoldierAllTime, object>>
+	> DefaultSortField => new("averageDpm", s => s.AverageDpm!);
+
+	public override SortOrder DefaultSortOrder => SortOrder.desc;
+
+	public SoldierAllTimeStatsRepository(
+		TF2SADbContext dbContext,
+		ILogger<SoldierAllTimeStatsRepository> logger
+	) : base(dbContext, logger) { }
+}
---
+++ dotnet/TF2SA.Data/Repositories/MariaDb/Statistics/SoldierRecentStatsRepository.cs
@@ -0,0 +1,57 @@
+using System.Linq.Expressions;
+using Microsoft.Extensions.Logging;
+using TF2SA.Data.Entities.MariaDb;
+using static TF2SA.Data.Extensions.TF2SALinqExtensions;
+
+namespace TF2SA.Data.Repositories.MariaDb.Statistics;
+
+public class SoldierRecentStatsRepository : StatsRepository<SoldierRecent>
+{
+	public override string UpdateProcQuery =>
+		"CALL UpdateSoldierRecentStats();";
+
+	public override Dictionary<
+		string,
+		Expression<Func<SoldierRecent, object>>
+	> PropertyKeySelectors =>
+		new()
+		{
+			{ "steamId", s => s.SteamId },
+			{ "playerName", s => s.PlayerName! },
+			{ "avatar", s => s.Avatar! },
+			{ "numberOfGames", s => s.NumberOfGames! },
+			{ "wins", s => s.Wins! },
+			{ "draws", s => s.Draws! },
+			{ "losses", s => s.Losses! },
+			{ "averageDpm", s => s.AverageDpm! },
+			{ "averageKills", s => s.AverageKills! },
+			{ "averageAssists", s => s.AverageAssists! },
+			{ "averageDeaths", s => s.AverageDeaths! },
+			{ "averageAirshots", s => s.AverageAirshots! },
+			{ "averageDamageTakenPm", s => s.AverageDamageTakenPm! },
+			{ "averageHealsReceivedPm", s => s.AverageHealsReceivedPm! },
+			{ "averageMedKitsHp", s => s.AverageMedKitsHp! },
+			{
+				"averageCapturePointsCaptured",
+				s => s.AverageCapturePointsCaptured!
+			},
+			{ "topKills", s => s.TopKills! },
+			{ "topKillsGameId", s => s.TopKillsGameId! },
+			{ "topDamage", s => s.TopDamage! },
+			{ "topDamageGameId", s => s.TopDamageGameId! },
+			{ "topAirshots", s => s.TopAirshots! },
+			{ "topAirshotsGameId", s => s.TopAirshotsGameId! },
+		};
+
+	public override Tuple<
+		string,
+		Expression<Func<SoldierRecent, object>>
+	> DefaultSortField => new("averageDpm", s => s.AverageDpm!);
+
+	public override SortOrder DefaultSortOrder => SortOrder.desc;
+
+	public SoldierRecentStatsRepository(
+		TF2SADbContext dbContext,
+		ILogger<SoldierRecentStatsRepository> logger
+	) : base(dbContext, logger) { }
+}
