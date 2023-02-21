c05259e79212615973f17f670d245007abb228c6
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-21 08:42:28 +0200

    call proc for scout all time

--- dotnet/TF2SA.StatsETLService/LogsTFIngestion/Services/StatisticsUpdater.cs
+++ dotnet/TF2SA.StatsETLService/LogsTFIngestion/Services/StatisticsUpdater.cs
@@ -2,21 +2,25 @@ using Monad;
 using TF2SA.Common.Errors;
 using TF2SA.Data.Entities.MariaDb;
 using TF2SA.Data.Repositories.Base;
+using TF2SA.StatsETLService.LogsTFIngestion.Errors;
 
 namespace TF2SA.StatsETLService.LogsTFIngestion.Services;
 
 public class StatisticsUpdater : IStatisticsUpdater
 {
 	private readonly IStatsRepository<ScoutRecent> scoutRecentStatsRepository;
+	private readonly IStatsRepository<ScoutAllTime> scoutAllTimeStatsRepository;
 	private readonly ILogger<StatisticsUpdater> logger;
 
 	public StatisticsUpdater(
 		IStatsRepository<ScoutRecent> scoutRecentStatsRepository,
-		ILogger<StatisticsUpdater> logger
+		ILogger<StatisticsUpdater> logger,
+		IStatsRepository<ScoutAllTime> scoutAllTimeStatsRepository
 	)
 	{
 		this.scoutRecentStatsRepository = scoutRecentStatsRepository;
 		this.logger = logger;
+		this.scoutAllTimeStatsRepository = scoutAllTimeStatsRepository;
 	}
 
 	public async Task<OptionStrict<Error>> UpdateAggregatedStatistics(
@@ -24,6 +28,7 @@ public class StatisticsUpdater : IStatisticsUpdater
 	)
 	{
 		logger.LogInformation("Updating Aggregated statistics.");
+		List<Error> errors = new();
 
 		OptionStrict<Error> updateScoutRecent =
 			await scoutRecentStatsRepository.CallUpdateStoredProc(
@@ -31,9 +36,24 @@ public class StatisticsUpdater : IStatisticsUpdater
 			);
 		if (updateScoutRecent.HasValue)
 		{
-			return updateScoutRecent.Value;
+			errors.Add(updateScoutRecent.Value);
 		}
 
+		OptionStrict<Error> updateScoutAllTime =
+			await scoutAllTimeStatsRepository.CallUpdateStoredProc(
+				cancellationToken
+			);
+		if (updateScoutRecent.HasValue)
+		{
+			errors.Add(updateScoutAllTime.Value);
+		}
+
+		if (errors.Any())
+		{
+			return new IngestionError(
+				string.Join(Environment.NewLine, errors.Select(e => e.Message))
+			);
+		}
 		return OptionStrict<Error>.Nothing;
 	}
 }
