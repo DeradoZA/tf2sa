b49b8a2eeeeb86dd437971f41966956817d365f3
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-21 16:26:16 +0200

    add soldier page, and links to top stats

--- frontend/src/app/services/stats/stats.service.ts
+++ frontend/src/app/services/stats/stats.service.ts
@@ -9,6 +9,7 @@ import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { DEFAULT_PAGE_SIZE } from '../httpConstants';
 import { GetScoutStatsResult } from './getScoutStats';
+import { GetSoldierStatsResult } from './getSoldierStats';
 
 const STATS_BASE_URL = `${environment.backendUrl}/Statistics`;
 
@@ -44,6 +45,40 @@ export class StatsService {
 		}
 		return this.httpClient
 			.get<GetScoutStatsResult>(`${STATS_BASE_URL}/ScoutRecent`, {
+				params: params,
+			})
+			.pipe(catchError(this.handleError));
+	}
+
+	getSoldierStats(
+		count = DEFAULT_PAGE_SIZE,
+		offset = 0,
+		sort = 'averageDpm',
+		sortorder = 'desc',
+		filterField = 'playerName',
+		filterValue = '',
+		period = 'recent'
+	) {
+		let params = new HttpParams();
+		params = params.append('count', count.toString());
+		params = params.append('offset', offset.toString());
+		params = params.append('sort', sort);
+		params = params.append('sortorder', sortorder);
+		params = params.append('filterField', filterField);
+		params = params.append('filterValue', filterValue);
+
+		if (period === 'alltime') {
+			return this.httpClient
+				.get<GetSoldierStatsResult>(
+					`${STATS_BASE_URL}/SoldierAllTime`,
+					{
+						params: params,
+					}
+				)
+				.pipe(catchError(this.handleError));
+		}
+		return this.httpClient
+			.get<GetSoldierStatsResult>(`${STATS_BASE_URL}/SoldierRecent`, {
 				params: params,
 			})
 			.pipe(catchError(this.handleError));
