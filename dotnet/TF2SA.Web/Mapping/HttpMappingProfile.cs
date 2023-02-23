0303d623816abe411e614807fa28cb8f7c3ef7b9
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-02-23 14:23:44 +0200

    add http models

--- dotnet/TF2SA.Web/Mapping/HttpMappingProfile.cs
+++ dotnet/TF2SA.Web/Mapping/HttpMappingProfile.cs
@@ -3,11 +3,13 @@ using TF2SA.Common.Models.Core;
 using TF2SA.Query.Queries.GetPlayers;
 using TF2SA.Query.Queries.Statistics.Demoman;
 using TF2SA.Query.Queries.Statistics.Medic;
+using TF2SA.Query.Queries.Statistics.Overall;
 using TF2SA.Query.Queries.Statistics.Scout;
 using TF2SA.Query.Queries.Statistics.Soldier;
 using TF2SA.Web.Controllers.V1.Players.Models;
 using TF2SA.Web.Controllers.V1.Statistics.Demoman;
 using TF2SA.Web.Controllers.V1.Statistics.Medic;
+using TF2SA.Web.Controllers.V1.Statistics.Overall;
 using TF2SA.Web.Controllers.V1.Statistics.Scout;
 using TF2SA.Web.Controllers.V1.Statistics.Soldier;
 
@@ -31,5 +33,8 @@ public class HttpMappingProfile : Profile
 
 		CreateMap<GetMedicStatsResult, GetMedicStatsHttpResult>();
 		CreateMap<MedicStatDomain, MedicStatHttpResult>();
+
+		CreateMap<GetOverallStatsResult, GetOverallStatsHttpResult>();
+		CreateMap<OverallStatDomain, OverallStatHttpResult>();
 	}
 }
