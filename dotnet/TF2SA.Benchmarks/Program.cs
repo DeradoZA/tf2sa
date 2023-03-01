048dbb887d24c5990f5bf77f49621dfcfe241a59
Adam Coetzee <adam.coetzee@angloamerican.com>
2023-03-01 09:42:35 +0200

    add benchmark testing steamid creation

---
+++ dotnet/TF2SA.Benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿using BenchmarkDotNet.Running;
+using TF2SA.Benchmarks.Common.Models.LogsTF.GameLogModel;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var summary = BenchmarkRunner.Run<PlayerBenchmarks>();
+	}
+}
